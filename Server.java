476ed6f58a56d2f77ee0f5518fb9d2ea8ac1556e
Leirda01 <adrieldj@orange.fr>
2020-01-09 16:47:49 +0100

    Créé un contexte / pour tester

--- Server.java
+++ Server.java
@@ -9,13 +9,26 @@ public class Server {
 	public static void main(String[] args) throws Exception {
 		HttpServer oiseau_social = HttpServer.create(new InetSocketAddress(8000), 0);
 		oiseau_social.createContext("/login", new LoginContext());
+    oiseau_social.createContext("/", new RootContext());
 		oiseau_social.setExecutor(null); // creates a default executor
 		oiseau_social.start();
 	}
 
+	static class RootContext implements HttpHandler {
+		@Override
+		public void handle(HttpExchange exchange) throws IOException {
+      String coucou = "Hello world!";
+
+      exchange.sendResponseHeaders(200, coucou.length());
+      OutputStream os = exchange.getResponseBody();
+      os.write(coucou.getBytes());
+      os.close();
+    }
+  }
+
 	static class LoginContext implements HttpHandler {
 		@Override
-		public void handle(HttpExchange request) throws IOException {
+		public void handle(HttpExchange exchange) throws IOException {
 
       Path file_path = FileSystems.getDefault().getPath("html", "login.html");
       BufferedReader file_reader = Files.newBufferedReader(file_path);
@@ -26,8 +39,8 @@ public class Server {
         login += line;
       }
 
-			request.sendResponseHeaders(200, login.length());
-			OutputStream os = request.getResponseBody();
+			exchange.sendResponseHeaders(200, login.length());
+			OutputStream os = exchange.getResponseBody();
 			os.write(login.getBytes());
 			os.close();
 		}
